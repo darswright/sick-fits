a95152d58aa12ee2e2e5dcf4a5e63018d3d76fc5
Simon Wright <simmo.wright@outlook.com>
2021-04-16 10:17:29 +1000

    Initial commit

---
+++ frontend/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Link from 'next/link';
+import Nav from './Nav';
+
+export default function Header() {
+  return (
+    <header>
+      <div className="bar">
+        <Link href="/">sick fits</Link>
+      </div>
+      <div className="sub-bar">
+        <p>search</p>
+      </div>
+      <Nav />
+    </header>
+  );
+}
---
+++ frontend/components/Page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Header from './Header';
+
+export default function Page({ children }) {
+  return (
+    <div>
+      <Header />
+      <h2>I am the page comp</h2>
+      {children}
+    </div>
+  );
+}
+
+Page.propTypes = {
+  children: PropTypes.any,
+};
